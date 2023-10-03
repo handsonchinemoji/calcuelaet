5c55d549a9c947c1905e0511371791b71319bb41
antonio <anto-jay@pm.me>
2023-10-02 17:28:33 -0700

    Initial Commit

---
+++ calcuelaet/calcuelaetApp.swift
@@ -0,0 +1,17 @@
+//
+//  calcuelaetApp.swift
+//  calcuelaet
+//
+//  Created by Antonio Gurrola-Beltran on 10/2/23.
+//
+
+import SwiftUI
+
+@main
+struct calcuelaetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
