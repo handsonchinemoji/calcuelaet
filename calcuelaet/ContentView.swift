211f993fcd7818752c34c828956f5d459162cae8
antonio <anto-jay@pm.me>
2023-11-16 01:05:09 -0800

    initial commit

--- calcuelaet/ContentView.swift
+++ calcuelaet/ContentView.swift
@@ -8,16 +8,69 @@
 import SwiftUI
 
 struct ContentView: View {
+    @ObservedObject var calcState = CalculatorState()
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        NavigationStack{
+            VStack {
+                Text(">\t\(calcState.calculatorState)\t<")
+            }
+            .padding()
+            // Container that detects swipe actions
+            // Swip container Left (VStack to left), or Right (VStack to Right)
+            // + Swipe container UP (Turns into Hstack, goes up), or DOWN (HStack, goes down)
+            VStack{
+                Button{
+                    calcState.add()
+                } label: {
+                    Image(systemName: "plus")
+                }
+                Spacer().frame(height: 15)
+                Button {
+                    calcState.subtract()
+                } label: {
+                    Image(systemName: "minus")
+                }
+                Spacer().frame(height: 15)
+                Button {
+                    calcState.divide()
+                } label: {
+                    Image(systemName: "divide")
+                }
+                Spacer().frame(height: 15)
+                Button {
+                    calcState.multiply()
+                } label: {
+                    Image(systemName: "multiply")
+                }
+            }
+            .padding()
         }
-        .padding()
+        .navigationTitle("C4LCUL8")
     }
 }
+
+class CalculatorState: ObservableObject {
+    @Published var calculatorState:Double = 0.0
+    
+    func add() {
+        calculatorState += 1
+    }
+    
+    func subtract() {
+        calculatorState -= 1
+    }
+    
+    func divide() {
+        calculatorState = calculatorState * 0.5
+    }
+    
+    func multiply() {
+        calculatorState = calculatorState * 2
+    }
+}
+
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
